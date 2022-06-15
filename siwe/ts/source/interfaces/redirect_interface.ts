1c69dae7cdc3053a4264be9f222c8629a2115afc
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-06-15 20:58:29 -0300

    Update redirect and redirect interface

--- siwe/ts/source/interfaces/redirect_interface.ts
+++ siwe/ts/source/interfaces/redirect_interface.ts
@@ -1,7 +1,5 @@
 export interface RedirectInterface {
     readonly url : string;
 
-    validate_url() : boolean;
-
     run_redirect() : void;
 }
--- siwe/ts/source/redirect.ts
+++ siwe/ts/source/redirect.ts
@@ -1,14 +1,10 @@
 import { RedirectInterface } from "./interfaces/redirect_interface";
 
-class Redirect implements RedirectInterface {
+export class Redirect implements RedirectInterface {
     readonly url : string;
 
     constructor(url : string) {
         this.url = url;
-    }
-
-    validate_url() : boolean {
-        return true;
     }
 
     run_redirect() : void {
